3dcb7068e3e0e9c1aeca860fcc9eb883e173b50c
Filip Lazovic <github.grumbling276@passinbox.com>
2024-10-11 19:03:35 +0200

    Update script

--- install-sdk.sh
+++ install-sdk.sh
@@ -1,11 +1,18 @@
 if [[ $(cargo --version) ]]; then
-    echo "Found cargo"
+    echo "Found cargo\n"
 else
     (curl -sSf https://sh.rustup.rs || echo "exit 1") | sh -s -- -y || exit
     source $HOME/.cargo/env || exit
     export PATH=$HOME/.cargo/bin:$PATH
 fi
 
-cargo install openrank-sdk --version "0.1.0"
+echo "Downloading Example Datasets..."
+git lfs pull
+echo "Done\n"
 
+echo "Installing SDK.."
+cargo install openrank-sdk --version "0.1.0"
+echo "Done\n"
+
+echo "Generating keypair:"
 openrank-sdk generate-keypair
